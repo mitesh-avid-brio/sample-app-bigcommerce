13d58fdfbbb6123d2ce66be9eaa204af44c1f1be
Arjun Poudel <arjunpoudel703@gmail.com>
2024-01-03 11:54:50 +0545

    >>>

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { Panel, Table, Button, Text, Box, ProgressBar } from '@bigcommerce/big-design';
 
+import{ ChevronRightIcon , ChevronLeftIcon } from  '@bigcommerce/big-design-icons'
 // const baseURL = "https://stellular-naiad-f9e846.netlify.app"
 // const baseURL = "http://localhost:3000"
 
@@ -9,9 +11,12 @@ const transactionsPage = () => {
   const [transactions, setTransactions] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [fromOrders, setFromOrders] = useState(0)
+  const [isLoading, setIsLoading] = useState(true)
 
   const fetchTransactions = async (page) => {
     try {
+      setIsLoading(true)
+
       const response = await fetch(`/api/transactions?page=${page}`);
 
       if (!response.ok) {
@@ -22,12 +27,12 @@ const transactionsPage = () => {
       let transactionData  = fetchedData.transactionData
       setTransactions(transactionData);
       setFromOrders(parseInt(fetchedData.fromOrders))
+      setIsLoading(false)
       console.log(fetchedData)
     } catch (error) {
       console.error('Error fetching transactions:', error);
     }
   };
-
   useEffect(() => {
     fetchTransactions(currentPage);
   }, [currentPage]);
@@ -38,58 +43,68 @@ const transactionsPage = () => {
     }
   };
 
-const handleNextPage = () => {
-  if (fromOrders === 50) {
-    setCurrentPage(currentPage + 1);
-  }
-};
+	const handleNextPage = () => {
+	  if (fromOrders === 50) {
+	    setCurrentPage(currentPage + 1);
+	  }
+	};
 
 
   return (
     <div>
-      <h1>transactions</h1>
-	    <table>
-	      <thead>
-	        <tr>
-	        	<th>Order ID</th>
-	          <th>Date Created</th>
-	          <th>Email</th>
-	          <th>Customer Name</th>
-	          <th>Amount</th>
-	          <th>Transaction ID</th>
-	          <th>Method</th>
-	          <th>Gateway</th>
-	          <th>Card Type</th>
-	          <th>AVS Result</th>
-	        </tr>
-	      </thead>
-	      <tbody>
-	        {transactions.map(transaction => (
-	          <tr key={transaction.id}>
-	          	<td>{transaction.order_id}</td>
-	            <td>{transaction.date_created}</td>
-	            <td>{transaction?.email}</td>
-	            <td>{transaction?.customerName}</td>
-	            <td>{transaction.amount}</td>
-	            <td>{transaction.id}</td>
-	            <td>{transaction.method}</td>
-	            <td>{transaction.gateway}</td>
-	            <td>{transaction.credit_card?.card_type }</td>
-	            <td>{transaction.avs_result.message}</td>
-	          </tr>
-	        ))}
-	      </tbody>
-	    </table>
-      <div>
-        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span> Page {currentPage} </span>
-        <button onClick={handleNextPage} disabled={fromOrders < 50}>
-          Next
-        </button>
-      </div>
-    </div>
+
+      {isLoading ? (
+        <Box marginVertical="large">
+          <ProgressBar />
+        </Box>
+      ) : (
+
+
+				<Panel
+				  header="Order Transaction Details"
+				>
+					<Table
+					  columns={[
+					    {
+					      header: 'Order ID', hash: 'order_id', render: ({ order_id }) => order_id
+					    },	
+					    {  header: 'Date Created', hash: 'date_created', render: ({ date_created }) => date_created
+					    },	
+					    {  header: 'Email', hash: 'email', render: ({ email }) => email
+					    },	
+					    {  header: 'Customer Name', hash: 'customerName', render: ({ customerName }) => customerName
+					    },	
+					    {  header: 'Amount', hash: 'amount', render: ({ amount }) => amount
+					    },	
+					    {  header: 'Transaction ID', hash: 'id', render: ({ id }) => id
+					    },	
+					    {  header: 'Method', hash: 'method', render: ({ method }) => method
+					    },	
+					    {  header: 'Gateway', hash: 'gateway', render: ({ gateway }) => gateway
+					    },	
+					    {  header: 'Card Type', hash: 'credit_card', render: ({ credit_card }) => credit_card?.card_type
+					    },	
+					    {  header: 'AVS Result', hash: 'avs_result', render: ({ avs_result }) => avs_result.message
+					    }	
+					    
+					  ]}
+					  items={transactions}
+					  stickyHeader
+					/>
+		      <div>
+		        <Button variant="utility"  onClick={handlePreviousPage} disabled={currentPage === 1}>
+		          <ChevronLeftIcon/>
+		        </Button>
+	  				<Button variant="utility">Page {currentPage}</Button>
+	  				<Button variant="utility" onClick={handleNextPage} disabled={fromOrders < 50}>
+		          <ChevronRightIcon  />
+	  				</Button>
+		      </div>
+				</Panel>
+
+      )}
+
+		</div>
   );
 };
 
